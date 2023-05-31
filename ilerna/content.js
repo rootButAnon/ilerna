6f2d65035c0a1966583e4c283f26ecc824dae898
rootButAnon <102680011+rootButAnon@users.noreply.github.com>
2023-05-31 14:24:33 +0200

    Add files via upload

--- ilerna/content.js
+++ ilerna/content.js
@@ -28,31 +28,73 @@ function readFile(fileName){
 
         questions.push({ question, answer });
     }
-    let counter = -1
+    let counter = 1
     pregTab = document.getElementById('tablaPreguntas')
     console.log(questions)
   questions.forEach(element => {
-    counter++
     pregunta = document.createElement('pregunta-component')
     pregunta.setAttribute('question',element.question)
     pregunta.setAttribute('answera',element.answer.a)
     pregunta.setAttribute('test-name',test)
     pregunta.setAttribute('numero',counter)
-
+    
     if(test == 'test1'){
-      pregunta.setAttribute('correctOne',correctAnswersTest1[counter])
-
+      pregunta.setAttribute('correctOne',correctAnswersTest1[counter-1])
+      if(counter == 13){
+        let parrafo = document.createElement ('div')
+        parrafo.innerHTML = `<p><b>Situación preguntas 13, 14 y 15</b></p>
+        <p><b>Un anciano llega al hospital con defecaciones muy escasas y dolorosas. Se
+        lo comentamos a la enfermera y ella nos dice que le administremos un
+        microenema.</b></p>`
+        pregTab.appendChild(parrafo)
+      }else if(counter == 18){
+        let parrafo = document.createElement ('div')
+        parrafo.innerHTML = `<p><b>Imagen pregunta 18</b></p>`
+        pregTab.appendChild(parrafo)
+        let img = document.createElement('img')
+        img.src = './img/test1-18.png'
+        parrafo.appendChild(img)    
+      }
     }else if(test == 'test2'){
-      pregunta.setAttribute('correctOne',correctAnswersTest2[counter])
-
+      pregunta.setAttribute('correctOne',correctAnswersTest2[counter-1])
+      if(counter == 13){
+        let parrafo = document.createElement ('div')
+        parrafo.innerHTML = `<p><b>Situación preguntas 13, 14 y 15</b></p>
+        <b>Estamos trabajando en el hospital de nuestra ciudad, nos comenta la enfermera que
+le tenemos que dar a Marcos una pastilla de paracetamol.</b> `
+        pregTab.appendChild(parrafo)
+      }else if(counter == 18){
+        let parrafo = document.createElement ('div')
+        parrafo.innerHTML = `<p><b>Imagen pregunta 18</b></p>`
+        pregTab.appendChild(parrafo)
+        let img = document.createElement('img')
+        img.src = './img/test2-18.png'
+        parrafo.appendChild(img)    
+      }
     }else if(test == 'test3'){
-      pregunta.setAttribute('correctOne',correctAnswersTest3[counter])
-
+      pregunta.setAttribute('correctOne',correctAnswersTest3[counter-1])
+      if(counter == 13){
+        let parrafo = document.createElement ('div')
+        parrafo.innerHTML = `<p><b>Situación preguntas 13, 14 y 15</b></p>
+        <p><b>Estamos trabajando en el hospital de nuestra ciudad, nos comenta la enfermera
+        que le tenemos que administrar un broncodilatador vía respiratoria mediante la
+        técnica con nebulizadores</b></p>`
+        pregTab.appendChild(parrafo)
+      }else if(counter == 18){
+        let parrafo = document.createElement ('div')
+        parrafo.innerHTML = `<p><b>Imagen pregunta 18</b></p>`
+        pregTab.appendChild(parrafo)
+        let img = document.createElement('img')
+        img.src = './img/test3-18.png'
+        parrafo.appendChild(img)     
+      }
     }
     pregunta.setAttribute('answerb',element.answer.b)
     pregunta.setAttribute('answerc',element.answer.c)
     pregunta.setAttribute('answerd',element.answer.d)
     pregTab.appendChild(pregunta)
+    
+    counter++
   })
 })
 
@@ -74,15 +116,25 @@ function corregirTest(event){
     let identificador = pregunta.getAttribute('test-name')+ '-' + pregunta.getAttribute('numero') + '-' + correcta
     let marcada = document.getElementById(identificador)
     if(marcada.checked != true){
-      incorrects.push(identificador.split('-')[1]+identificador.split('-')[2]) 
+      incorrects.push(identificador.split('-')[1]+identificador.split('-')[2])
+      let solution = document.createElement('div')
+      solution.className = 'solution'
+      pregunta.appendChild(solution)
+      let marcaIncorrecta = document.createElement('div')
+      marcaIncorrecta.className = 'crossCustom'
+      solution.appendChild(marcaIncorrecta) 
+
+      let textoIncorrecto = document.createElement('div')
+      textoIncorrecto.innerHTML= '<p>LA RESPUESTA CORRECTA ES LA ' + identificador.split('-')[2] + '</p>'
+      solution.appendChild(textoIncorrecto) 
     }
 
   })
   console.log(incorrects)
     incorrects.forEach(incorrecta => {
-      document.getElementById('resultado').innerHTML += 'Nº de Pregunta: '+ incorrecta.substring(0,incorrecta.length -1) + ' , Respuesta: ' + incorrecta.substring(incorrecta.length-1,incorrecta.length) + '  ||  '
+      //document.getElementById('resultado').innerHTML += 'Nº de Pregunta: '+ incorrecta.substring(0,incorrecta.length -1) + ' , Respuesta: ' + incorrecta.substring(incorrecta.length-1,incorrecta.length) + '  ||  '
     })
-    document.body.scrollIntoView({ behavior: 'auto', block: 'end' });
+    
 
   
 }
